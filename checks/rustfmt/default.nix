36859bbe72759487a0aa7236e27ba562fbce0d6f
Guillaume Friloux <guillaume@friloux.me>
2026-05-03 18:01:41 +0200

    chore(nix): fix checks by replacing rustfmt with cargo fmt
    fix checks by replacing rustfmt with cargo fmt
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- checks/rustfmt/default.nix
+++ checks/rustfmt/default.nix
@@ -3,6 +3,6 @@ pkgs.runCommand "rustfmt-check" {
   nativeBuildInputs = [pkgs.rustfmt];
 } ''
   cp -r ${../../src} ./src
-  rustfmt --check --config tab_spaces=2 ./src/*.rs
+  find ./src -name "*.rs" | xargs rustfmt --check --config tab_spaces=2 --edition 2021
   touch $out
 ''
