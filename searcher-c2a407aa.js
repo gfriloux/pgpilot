8ea87c6edb84db7ef6e34e049c6c60564eee8603
gfriloux <gfriloux@users.noreply.github.com>
2026-05-08 13:51:05 +0000

    deploy: e95ceb5992b99e642a38617bd3d49d1f5c8f3be2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ff1e9e42.js',
+                path_to_root + 'searchindex-a5fbf968.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
